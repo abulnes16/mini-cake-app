b7c78481dcdef161e1dd1275435167ab74fde22a
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-09-30 20:46:29 -0600

    Initial data

---
+++ app/src/main/java/com/example/minicakeapp/ui/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.minicakeapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.minicakeapp.R
+import com.example.minicakeapp.databinding.FragmentHomeBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val fragmentBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.btnOrder?.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_userFragment)
+        }
+    }
+
+
+}
